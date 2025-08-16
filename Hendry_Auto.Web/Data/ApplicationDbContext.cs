97b1af403eb0400ba2c2f8231d313c6905ffc0fd
arun kumar <type8meenu@gmail.com>
2025-08-16 12:03:11 +0530

    Add project files.

---
+++ Hendry_Auto.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hendry_Auto.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Hendry_Auto.Web.Models.Brand> Brands { get; set; }
+    }
+}
